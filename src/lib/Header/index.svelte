85c0545c54da9958e60c81f43cca06d309aab410
wighawag <wighawag@gmail.com>
2021-05-17 20:31:03 +0100

    test with ipfs adapter

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import {base} from '$app/paths';
 	import { page } from '$app/stores';
 	import logo from './svelte-logo.svg';
 </script>
@@ -6,7 +7,7 @@
 <header>
 	<div class="corner">
 		<a href="https://kit.svelte.dev">
-			<img src={logo} alt="SvelteKit" />
+			<img src={`${base}${logo}`} alt="SvelteKit" />
 		</a>
 	</div>
 
@@ -15,9 +16,9 @@
 			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
 		</svg>
 		<ul>
-			<li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-			<li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
-			<li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
+			<li class:active={$page.path === `${base}/`}><a sveltekit:prefetch href={`${base}/`}>Home</a></li>
+			<li class:active={$page.path === `${base}/about/`}><a sveltekit:prefetch href={`${base}/about/`}>About</a></li>
+			<!-- <li class:active={$page.path === `${base}/todos/`}><a sveltekit:prefetch href="/todos">Todos</a></li> -->
 		</ul>
 		<svg viewBox="0 0 2 3" aria-hidden="true">
 			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
